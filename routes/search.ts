45c82d64f1c25100fef048355a1f34458da525a2
Pargat Singh <106462501+paajii@users.noreply.github.com>
2024-10-28 19:40:06 +1030

    Login & Search & Testing File
    login and and search file SQL Injection patched re added to new repo for Pull request.
    Testing.yml file also added works exactly as expected

--- routes/search.ts
+++ routes/search.ts
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
@@ -6,6 +7,7 @@
 import models = require('../models/index')
 import { Request, Response, NextFunction } from 'express'
 import { UserModel } from '../models/user'
+import { ParsedQs } from 'qs' // Importing ParsedQs for type safety
 
 const utils = require('../lib/utils')
 const challengeUtils = require('../lib/challengeUtils')
@@ -18,11 +20,35 @@ class ErrorWithParent extends Error {
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
-      .then(([products]: any) => {
+    // Handle undefined or empty query parameter `q`
+    let criteria: string = (req.query.q as string) === 'undefined' || !req.query.q ? '' : (req.query.q as string)
+
+    // Truncate criteria if it's too long
+    criteria = criteria.length <= 200 ? criteria : criteria.substring(0, 200)
+
+    // Prepare the SQL query and replacements based on whether criteria is empty
+    let query
+    let replacements
+
+    if (criteria === '') {
+      // No search criteria, show all products
+      query = 'SELECT * FROM Products WHERE deletedAt IS NULL ORDER BY name'
+      replacements = {} 
+    } else {
+      // Search products based on the criteria
+      query = 'SELECT * FROM Products WHERE (name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL ORDER BY name'
+      replacements = { criteria: `%${criteria}%` } // Safely insert search criteria
+    }
+
+    models.sequelize.query(query, {
+      replacements, // Use replacements for safely inserting criteria
+      type: models.sequelize.QueryTypes.SELECT
+    })
+      .then((products: any) => {
+        console.log('Products returned:', products) // Debugging line
         const dataString = JSON.stringify(products)
+
+        // Challenge checks
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
           let solved = true
           UserModel.findAll().then(data => {
@@ -42,6 +68,8 @@ module.exports = function searchProducts () {
             next(error)
           })
         }
+
+        // Check for database schema challenge
         if (challengeUtils.notSolved(challenges.dbSchemaChallenge)) {
           let solved = true
           models.sequelize.query('SELECT sql FROM sqlite_master').then(([data]: any) => {
@@ -59,12 +87,16 @@ module.exports = function searchProducts () {
             }
           })
         } // vuln-code-snippet hide-end
+
+        // Localize product names and descriptions before returning
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
         }
-        res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
+
+        res.json(utils.queryResultToJson(products)) // Return products as JSON
+      })
+      .catch((error: ErrorWithParent) => {
         next(error.parent)
       })
   }
